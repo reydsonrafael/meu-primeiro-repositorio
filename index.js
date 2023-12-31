eb0b330764d75d4b59fd68bcd9699b44f09897b5
reysdonrafael <reydsonrafael79495@email.com>
2023-12-30 23:17:54 -0300

    projeto criado

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-console.log("olá mundão");
-console.log("helooooo");
+//for (let i = 0; i < 20; i++){
+   // console.log(`contagem ${i}`)};
+
+let saboresDeSorvetes = ["chocolate", "creme", "danone", "napolitano"];
+let pedido= "chocolate";
+
+
+switch (pedido) {
+    case saboresDeSorvetes[0]: 
+console.log("boa escolha, o sabor " + saboresDeSorvetes[0] + " é uma delicia!")
+break
+    case saboresDeSorvetes[1]:
+ console.log("boa escolha, o sabor " + saboresDeSorvetes[1] + " é uma delicia!")
+break
+    case saboresDeSorvetes[2]:
+     console.log("boa escolha, o sabor " + saboresDeSorvetes[2] + " é uma delicia!")
+break
+    case saboresDeSorvetes[3]: console.log("boa escolha, o sabor " + saboresDeSorvetes[3] + " é uma delicia!")
+break
+default: console.log("tem não ó")}
+
+
+
+
+
