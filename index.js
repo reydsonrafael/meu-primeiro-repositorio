2724ee7e3994f9fad075711b36ca158eaf4c0336
reysdonrafael <reydsonrafael79495@email.com>
2023-12-31 00:39:19 -0300

    Desafio Concluido

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-//for (let i = 0; i < 20; i++){
-   // console.log(`contagem ${i}`)};
+//DESAFIO CLASIFICADOR DE NÍVEL DE HERÓI//
+//__________________________________//
 
-let saboresDeSorvetes = ["chocolate", "creme", "danone", "napolitano"];
-let pedido= "chocolate";
+const heroName = "Kurosaki Ichigo"
+let xp = 9999
+let msg = ", seu nivel atual é: "
+let msg2 = " treine para subir no ranking!"
+let msgEspecial = " Você atingiu o nivel max. agora és o melhor dos melhores!"
 
+if (xp <= 1000) { console.log(heroName + msg + "Ferro!"); console.log(msg2);}
 
-switch (pedido) {
-    case saboresDeSorvetes[0]: 
-console.log("boa escolha, o sabor " + saboresDeSorvetes[0] + " é uma delicia!")
-break
-    case saboresDeSorvetes[1]:
- console.log("boa escolha, o sabor " + saboresDeSorvetes[1] + " é uma delicia!")
-break
-    case saboresDeSorvetes[2]:
-     console.log("boa escolha, o sabor " + saboresDeSorvetes[2] + " é uma delicia!")
-break
-    case saboresDeSorvetes[3]: console.log("boa escolha, o sabor " + saboresDeSorvetes[3] + " é uma delicia!")
-break
-default: console.log("tem não ó")}
+else if (xp <= 2000) {console.log(heroName + msg + "Bronze!"); console.log(msg2);}
 
+else if (xp <= 5000) {console.log(heroName + msg + "Prata Ouro!"); console.log(msg2);}
 
+else if (xp <= 8000) {console.log(heroName + msg + "Platina Diamante!"); console.log(msg2);}
 
+else if (xp <= 9000) {console.log(heroName + msg + "Ascendente!"); console.log(msg2);}
 
+else if (xp <= 10000) {console.log(heroName + msg + "Imortal!"); console.log(msg2);}
 
+else if (xp > 10001) {console.log(heroName + msg + "Deus!"); console.log(msgEspecial);}
